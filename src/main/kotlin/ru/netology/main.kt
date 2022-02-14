941291067d9e9d2ea2793c7e3a04b54f463b8fd0
Ilya <onischuk2007@ya.ru>
2022-02-14 12:20:05 +0300

    Commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,9 +3,14 @@ package ru.netology
 import java.util.*
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val likes = scanner.nextInt()
-    val designationOfPeople = if (likes == 1) "человеку" else "человекам"
+
+    var likes = 121
+    val designationOfPeople = when (likes) {
+        1, 21, 31, 41, 51, 61, 71, 81, 91, 101 -> "человеку"
+
+        else -> "людям"
+    }
+
     println("Понравилось $likes $designationOfPeople")
 }
 
