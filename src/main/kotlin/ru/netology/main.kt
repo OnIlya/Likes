1a0f8d7b2d683ef569030dac143c6e5198880c5c
Ilya <onischuk2007@ya.ru>
2022-02-14 12:21:21 +0300

    Commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main() {
 
-    var likes = 121
+    var likes = 21
     val designationOfPeople = when (likes) {
         1, 21, 31, 41, 51, 61, 71, 81, 91, 101 -> "человеку"
 
